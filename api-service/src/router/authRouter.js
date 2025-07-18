5b6f538b9c2c747774c59dfb9895bdcfc1b56881
Nelson Albera <nalbera@hotmail.com>
2025-07-18 02:03:58 -0300

    agrego login

--- api-service/src/router/authRouter.js
+++ api-service/src/router/authRouter.js
@@ -1,10 +1,42 @@
 import { Router } from "express";
+import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+
+import AuthUsers from "../database/models/AuthUsers.js";
+import loginUserController from "../controllers/authusers/loginUserController.controller.js";
 
 const authRouter = Router();
 
-authRouter.get('/login', (req, res, next) => {
-    res.send('Soy el login');
+dotenv.config();
+
+
+authRouter.get('/look', async (req, res) => {
+
+    const {USER, PASSWORD} = process.env;
+
+    try {
+        
+        const passwordEncrypt = await bcrypt.hash(PASSWORD, 10);
+
+        const authUser = {
+            firstName: 'Soy',
+            lastName: 'Solo yo',
+            userName: USER,
+            password: passwordEncrypt
+        }
+
+        await AuthUsers.create(authUser);
+
+        res.send({
+            message: 'AuthUser creado correctamente....'
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
 })
+
+authRouter.post('/login', loginUserController);
 
 
 export default authRouter;
